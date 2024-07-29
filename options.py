e8438a034c9ca4c6ba716f106e62ee6e3a0a36ab
Guilherme Marinho de Araujo <109555952+gui98araujo@users.noreply.github.com>
2024-07-29 17:20:39 -0300

    Update options.py

--- options.py
+++ options.py
@@ -42,6 +42,7 @@ def VaR():
 
     data = yf.download(escolha, start='2013-01-01', end='2025-01-01')
 
+    
     current_price = data['Adj Close'][-1]
 
     data_fim = st.date_input('Selecione a data final:', datetime.now())
