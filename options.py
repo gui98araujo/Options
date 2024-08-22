430e5cfc15bc746fd324090a689a91e5f0141d1c
Guilherme Marinho de Araujo <109555952+gui98araujo@users.noreply.github.com>
2024-08-22 08:08:30 -0300

    Update options.py

--- options.py
+++ options.py
@@ -1308,8 +1308,8 @@ def blackscholes():
     risk_free_rate = 0.053
 
     volatilities = {
-        'SBV24.NYB': 0.2543,
-        'SBH25.NYB': 0.2353
+        'SBV24.NYB': 0.2500,
+        'SBH25.NYB': 0.2418
     }
 
     # Interface do Streamlit
